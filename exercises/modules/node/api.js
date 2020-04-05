0fd4fc7bb83432d850f4aa0eea0cab5754994000
moraispedro <pedromtmorais@gmail.com>
2020-04-05 13:33:35 +0100

    formatted code

--- exercises/modules/node/api.js
+++ exercises/modules/node/api.js
@@ -1,19 +1,19 @@
-const {users, posts} = require('./data')
+const { users, posts } = require('./data')
 
 getUserById = (id, cb) => {
-    setTimeout(() => {
-        const user = users.find(user => user.id === id)
-        cb(user)
-    }, 150)
+	setTimeout(() => {
+		const user = users.find(user => user.id === id)
+		cb(user)
+	}, 150)
 }
 
 getPostsForUser = (userId, cb) => {
-    setTimeout(() => {
-        const _posts = posts.filter(post => post.createdBy === userId)
-        cb(_posts)
-    }, 150)
+	setTimeout(() => {
+		const _posts = posts.filter(post => post.createdBy === userId)
+		cb(_posts)
+	}, 150)
 }
 
 module.exports = {
-    getUserById, getPostsForUser
+	getUserById, getPostsForUser
 }
